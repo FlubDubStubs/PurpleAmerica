8d31d6b9be22a24adab0ae5628b29380aa607dcb
hcps-mabrysh <hcps-mabrysh@180-17MABRYSH.hcps.internal>
2016-04-20 12:33:14 -0400

    Made LineDrawer class

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -18,8 +18,7 @@ import edu.princeton.cs.introcs.*;
  */
 public class PoliticalMap {
     public static void main(String[] args){
-        //comment
-        System.out.println("hello world");
+        
         
     }
 }
