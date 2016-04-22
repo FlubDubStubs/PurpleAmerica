b76588b8a23864e0f013018b3c23d10a5b5c8487
hcps-mabrysh <hcps-mabrysh@180-17MABRYSH.hcps.internal>
2016-04-22 14:30:45 -0400

    Made LineDrawer class

--- src/map/LineDrawer.java
+++ src/map/LineDrawer.java
@@ -5,11 +5,25 @@
  */
 
 package map;
-
+import edu.princeton.cs.introcs.*;
+import java.io.*;
+import java.util.Scanner;
 /**
  *
  * @author hcps-mabrysh
  */
 public class LineDrawer {
+    
+    public void draw(){
+        
+        System.out.print("Please enter a state's initials (ex. NY) and then .txt");
+        Scanner scanText = new Scanner(System.in);
+        String file = scanText.next();
+        File in = new File (file);
+        
+        StdDraw.setPenRadius(.005);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        
+    }//end draw
     
 }
