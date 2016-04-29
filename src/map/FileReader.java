9e9ad041e6163482e672452173b4a5d12431ee20
hcps-taylorsd <hcps-taylorsd@180-17TAYLORSD.hcps.internal>
2016-04-29 15:55:04 -0400

    I added the VA file as practice to read in a file and print it.

---
+++ src/map/FileReader.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package map;
+
+import java.io.File;
+import java.util.*;
+
+/**
+ *
+ * @author hcps-taylorsd
+ */
+public class FileReader {
+    
+    
+            Scanner scan = new Scanner(System.in);
+            File state = new File("VA.txt");
+            
+            //Trying to read the files and just print them for a test 
+            
+            scan.nextLine(state);
+            
+            System.out.println(state);
+            
+            
+            
+            
+    
+    
+    
+    
+}
